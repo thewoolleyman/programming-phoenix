9257684e5f6627e7cfeb9e959512798b3035afb5
Chad Woolley <thewoolleyman@gmail.com>
2017-05-14 21:51:30 -0700

    ch03 finished

--- rumbl/lib/rumbl/repo.ex
+++ rumbl/lib/rumbl/repo.ex
@@ -4,9 +4,9 @@ defmodule Rumbl.Repo do
   """
 #  use Ecto.Repo, otp_app: :rumbl
   def all(Rumbl.User) do
-    [ %Rumbl.User{id: 1, name: "José", username: "josevalim", password: "elixir"},
-      %Rumbl.User{id: 2, name: "Bruce", username: "redrapids", password: "7langs"},
-      %Rumbl.User{id: 2, name: "Chris", username: "chrismccord", password: "phx"}]
+    [ %Rumbl.User{id: "1", name: "José", username: "josevalim", password: "elixir"},
+      %Rumbl.User{id: "2", name: "Bruce", username: "redrapids", password: "7langs"},
+      %Rumbl.User{id: "3", name: "Chris", username: "chrismccord", password: "phx"}]
   end
 
   def all(_module), do: []
