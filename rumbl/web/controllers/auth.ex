a007f7acf17b9df8112b4af6ba0f922217b6cab5
Chad Woolley <thewoolleyman@gmail.com>
2017-05-16 15:18:51 -0700

    ch05 - user login

--- rumbl/web/controllers/auth.ex
+++ rumbl/web/controllers/auth.ex
@@ -5,6 +5,13 @@ defmodule Rumbl.Auth do
     Keyword.fetch!(opts, :repo)
   end
 
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
+  end
+
   def call(conn, repo) do
     user_id = get_session(conn, :user_id)
     user = user_id && repo.get(Rumbl.User, user_id)
