34ae02066930462fc2ff999c009a49a29b38cce5
Chad Woolley <thewoolleyman@gmail.com>
2017-05-28 17:41:27 -0700

    ch10 - socket authentication

--- rumbl/web/channels/user_socket.ex
+++ rumbl/web/channels/user_socket.ex
@@ -1,5 +1,6 @@
 defmodule Rumbl.UserSocket do
   use Phoenix.Socket
+  @max_age 2 * 7 * 24 * 60 * 60
 
   ## Channels
  channel "videos:*", Rumbl.VideoChannel
@@ -19,9 +20,16 @@ defmodule Rumbl.UserSocket do
   #
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
-  def connect(_params, socket) do
-    {:ok, socket}
+  def connect(%{"token" => token}, socket) do
+    case Phoenix.Token.verify(socket, "user socket", token, max_age: @max_age) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+      {:error, _reason} ->
+          :error
+    end
   end
+
+  def connect(_params, _socket), do: :error
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
   #
