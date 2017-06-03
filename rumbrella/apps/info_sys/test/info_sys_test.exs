effbb8946f2376b7c8d243c61ae098e0feb00f24
Chad Woolley <thewoolleyman@gmail.com>
2017-06-03 14:22:16 -0700

    ch13 - testing the information system - exceptions

--- rumbrella/apps/info_sys/test/info_sys_test.exs
+++ rumbrella/apps/info_sys/test/info_sys_test.exs
@@ -12,6 +12,13 @@ defmodule InfoSysTest do
     def fetch("none", ref, owner, _limit) do
       send(owner, {:results, ref, []})
     end
+    def fetch("timeout", _ref, owner, _limit) do
+      send(owner, {:backend, self()})
+      :timer.sleep(:infinity)
+    end
+    def fetch("boom", _ref, _owner, _limit) do
+      raise "boom!"
+    end
   end
 
   test "compute/2 with backend results" do
@@ -21,5 +28,22 @@ defmodule InfoSysTest do
 
   test "computer/2 with no backend results" do
     assert [] = InfoSys.compute("none", backends: [TestBackend])
+  end
+
+  test "compute/2 with timeout returns no results and kills workers" do
+    results = InfoSys.compute("timeout", backends: [TestBackend], timeout: 10)
+    assert results == []
+    assert_receive {:backend, backend_pid}
+    ref = Process.monitor(backend_pid)
+    assert_receive {:DOWN, ^ref, :process, _pid, _reason}
+    refute_received {:DOWN, _, _, _, _}
+    refute_received :timedout
+  end
+
+  @tag :capture_log
+  test "compute/2 discards backend errors" do
+    assert InfoSys.compute("boom", backends: [TestBackend]) == []
+    refute_received {:DOWN, _, _, _, _}
+    refute_received :timedout
   end
 end
